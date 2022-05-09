617cdd5a1e68e62288de09ec90e047c80170816e
Katarzyna Klimiuk <klimiuk.kasia@gmail.com>
2022-05-09 15:34:25 +0200

    pages decorator added

--- task3.py
+++ task3.py
@@ -5,35 +5,44 @@ from typing import Dict, List, Optional
 from requests import request
 
 DOMAIN = "https://recruitment.developers.emako.pl"
+PAGES = [1, 2, 3]
 
 
 class Connector:
     @lru_cache
     def headers(self) -> Dict[str, str]:
-        # reimplement as needed
         return {
             "Authorization": None,
             "Content-Type": "application/json",
             "Accept": "application/json",
         }
 
-    def request(self, method: str, path: str, data: dict = {}) -> dict:
+    def pages_decorator(self, pages):
+        def inner(func):
+            def wrapper():
+                for page in pages:
+                    value = func(page)
+                return value
+            return wrapper
+        return inner
+
+    @pages_decorator(pages=PAGES)
+    def request(self, method: str, path: str, page: int, data: dict = {}) -> dict:
         return request(
-            method, f"{DOMAIN}/{path}", json=data, headers=self.headers()
+            method, f"{DOMAIN}/{path}?page={page}", json=data, headers=self.headers()
         ).json()
 
     def get_products(self, ids: Optional[List[int]] = None) -> List[dict]:
-        return self.request("GET", "products", {"ids": ids, "pagination": {"page_size": 40}})["result"]
+        return self.request("GET", "products", {"ids": ids})["result"]
 
     def get_all_products_summary(self) -> List[dict]:
-        return self.request("GET", "products", {"detailed": False, "pagination": {"page_size": 40}})["result"]
+        return self.request("GET", "products", {"detailed": False})["result"]
 
     def get_new_products(self, newer_than: Optional[datetime] = None) -> List[dict]:
         if newer_than is None:
             newer_than = datetime.now() - timedelta(days=5)
         return self.request(
-            "GET", "products", {"created_at": {"start": newer_than.isoformat()}, "pagination": {"page_size": 40}}
-        )["result"]
+            "GET", "products", {"created_at": {"start": newer_than.isoformat()}})["result"]
 
     def add_products(self, products: List[dict]):
         units = 20
